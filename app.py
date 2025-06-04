acdb7306100ed04772b9de874455d55ad5f3b76f
Tanja <tanya.sevryukova@gmail.com>
2025-06-04 23:59:08 +0200

    Created a calculator app with addition and substraction

---
+++ app.py
@@ -0,0 +1,11 @@
+number_1 = input("Enter the first number: ")
+number_2 = input("Enter the second number: ")
+action = input("Choose action: '+' - addition, '-' - substraction: ")
+
+
+if action == '+':
+    print(int(number_1) + int(number_2))
+elif action == '-':
+    print(int(number_1) - int(number_2))
+else:
+    print('Try again')
